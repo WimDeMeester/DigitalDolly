3ef69e8513c95a25d31eaf5a1d140304e226a003
Wim De Meester <deepskywim@gmail.com>
2015-03-23 21:02:31 +0100

    Get the list of filenames in input_dir.

--- DigitalDolly.py
+++ DigitalDolly.py
@@ -1,7 +1,20 @@
 from PIL import Image
-im = Image.open('~/Pictures/IMG_20140320_072340.jpg')
+import os
 
-outfile = "sth2.jpg"
-region=im.crop((0, 0, 1920, 1080))
-#Do some operations here if you want but on region not on im!
-region.save(outfile, "JPEG")
+# The directory where all the images for the timelapse are stored.
+input_dir = "/lhome/wim/Pictures/"
+
+# The directory where the cropped images will be stored.
+output_dir = "~/Downloads/"
+
+# The filetypes to use.
+included_extentions = ['jpg','bmp','png','gif' ] ;
+
+file_names = [fn for fn in os.listdir(input_dir) if any([fn.endswith(ext) for ext in included_extentions])];
+print file_names
+#im = Image.open('~/Pictures/IMG_20140320_072340.jpg')
+
+#outfile = "sth2.jpg"
+#region=im.crop((0, 0, 1920, 1080))
+##Do some operations here if you want but on region not on im!
+#region.save(outfile, "JPEG")
