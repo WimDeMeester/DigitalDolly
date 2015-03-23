a8922fe8bc33b9fdb4b31d6fb6a965864aa43861
Wim De Meester <deepskywim@gmail.com>
2015-03-23 22:36:57 +0100

    Use floats to calculate the step between two images of the timelapse.

--- DigitalDolly.py
+++ DigitalDolly.py
@@ -2,10 +2,10 @@ from PIL import Image
 import os
 
 # The directory where all the images for the timelapse are stored.
-input_dir = "/lhome/wim/Downloads/test/in/"
+input_dir = "/Users/wim/Desktop/20150307-201512/"
 
 # The directory where the cropped images will be stored.
-output_dir = "/lhome/wim/Downloads/test/out/"
+output_dir = "/Users/wim/Desktop/out1/"
 
 # The filetypes to use.
 included_extentions = ['jpg','JPG','bmp','BMP','png','PNG','gif','GIF'] ;
@@ -26,15 +26,15 @@ first_x = resolution[0] - 1920
 final_y = 0
 final_x = 0
 
-step_x = (final_x - first_x) / (number_of_files - 1)
-step_y = (final_y - first_y) / (number_of_files - 1)
+step_x = (final_x - first_x) / (number_of_files - 1.)
+step_y = (final_y - first_y) / (number_of_files - 1.)
 
 count = 0
 for image in sorted(file_names):
     im=Image.open(input_dir + "/" + image)
     outfile = output_dir + "%07d.jpg" %count
-    x = first_x + step_x * count
-    y = first_y + step_y * count
+    x = (int)(first_x + step_x * count)
+    y = (int)(first_y + step_y * count)
     print "Cropping file " + str(count) + ": im.crop(%d, %d, %d, %d)" % (x, y, x + 1920, y + 1080)
     region=im.crop((x, y, x + 1920, y + 1080))
     region.save(outfile, "JPEG")
