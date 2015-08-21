7698bfb49e9512459ab6cd6e521ce82ca20937cb
Wim De Meester <deepskywim@gmail.com>
2015-08-21 11:19:03 +0200

    Corrected some small problems.

--- DigitalDolly.py
+++ DigitalDolly.py
@@ -1,8 +1,8 @@
 """
-PURPOSE: Prepare separate images for a timelapse to introduce movement of the camera. This 
+PURPOSE: Prepare separate images for a timelapse to introduce movement of the camera. This
           will crop the image to a resolution of 1920x1080. As a consequence, the movie
-          will be zoomed, but the script will travel through the image.  
-         
+          will be zoomed, but the script will travel through the image.
+
 USAGE: DigitalDolly.py --inputDir <inputDirectory> --outputDir <outputDirectory> --beginX <beginX> --beginY <beginY> --endX <endX> --endY <endY>
 
 INPUT: --inputDir : The directory where the input files for the timelapse are located.
@@ -12,7 +12,7 @@ INPUT: --inputDir : The directory where the input files for the timelapse are lo
        --endX : The left pixel where the movement will end. This should be between [0, maxX - 1920]. If this option is not given, the value will be 0.
        --endY : The top pixel where the movement will end. This should be between [0, maxY - 1080]. If this option is not given, the value will be 0.
 
-OUTPUT: Each image will be cropped and will be moved a bit in comparison with the previous image.  
+OUTPUT: Each image will be cropped and will be moved a bit in comparison with the previous image.
 
 """
 from PIL import Image
@@ -38,9 +38,14 @@ def dolly(input_dir=input_dir, output_dir=output_dir, first_x=first_x, first_y=f
     file_names = [fn for fn in os.listdir(input_dir) if any([fn.endswith(ext) for ext in included_extentions])];
     number_of_files = len(file_names)
     print "Number of files to process: " + str(number_of_files)
-    
+
     resolution = Image.open(input_dir + "/" + file_names[0]).size
     print "Original files have a resolution of " + str(resolution)
+
+    first_x = int(first_x)
+    first_y = int(first_y)
+    final_x = int(final_x)
+    final_y = int(final_y)
 
     # If first_x is not given, we start at the right side of the image.
     if (first_x < 0):
@@ -49,18 +54,18 @@ def dolly(input_dir=input_dir, output_dir=output_dir, first_x=first_x, first_y=f
     # If first_y is not given, we start at the bottom of the image.
     if (first_y < 0):
         first_y = resolution[1] - 1080
-    
+
     # If final_x is not given, we end at the left side of the image.
     if (final_x < 0):
         final_x = 0
-    
+
     # If final_y is not given, we end at the top of the image.
     if (final_y < 0):
         final_y = 0
-        
+
     step_x = (final_x - first_x) / (number_of_files - 1.)
     step_y = (final_y - first_y) / (number_of_files - 1.)
-    
+
     count = 0
     for image in sorted(file_names):
         im=Image.open(input_dir + "/" + image)
@@ -76,13 +81,13 @@ def dolly(input_dir=input_dir, output_dir=output_dir, first_x=first_x, first_y=f
 def main(argv):
     global input_dir, output_dir, first_x, first_y, final_x, final_y
     try:
-        opts, args = getopt.getopt(argv,"i:o:",["inputDir=","outputDir=","beginX=","beginy=","endX=", "endY="])
+        opts, args = getopt.getopt(argv,"i:o:",["inputDir=","outputDir=","beginX=","beginY=","endX=", "endY="])
     except getopt.GetoptError:
         print "Error parsing arguments"
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print "Prepare separate images for a timelapse to introduce movement of the camera. This will crop the image to a resolution of 1920x1080. As a consequence, the movie will be zoomed, but the script will travel through the image."  
+            print "Prepare separate images for a timelapse to introduce movement of the camera. This will crop the image to a resolution of 1920x1080. As a consequence, the movie will be zoomed, but the script will travel through the image."
             print ""
             print "USAGE: DigitalDolly.py --inputDir <inputDirectory> --outputDir <outputDirectory> --beginX <beginX> --beginY <beginY> --endX <endX> --endY <endY>"
             print ""
@@ -107,6 +112,7 @@ def main(argv):
         elif opt in ("--endY"):
             final_y = arg
 
+    print "TEST"
     dolly(input_dir, output_dir, first_x, first_y, final_x, final_y)
 
 if __name__ == "__main__":
